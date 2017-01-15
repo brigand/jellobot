8b1f6a8b2e0bb2b1360d40f3c99d682da5a0ea82
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-01-15 03:09:34 -0700

    implements live plugin loading and other frameworking

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,6 @@
 const irc = require('irc');
+const chalk = require('chalk');
+const {safeDump: yamlStringify} = require('js-yaml');
 const {readAndProcessConfig} = require('./utils/getConfig');
 const plugins = require('./plugins/plugins.js');
 
@@ -6,11 +8,31 @@ const config = readAndProcessConfig();
 
 const client = new irc.Client(config.server, config.nick, config.ircClientConfig);
 
+// mutable list of recent messages per channel
+// newest messages first
+const logs = {};
+
 client.addListener('message', (from, to, message) => {
   const messageObj = {
     from,
     message,
     config,
+  };
+
+  const say = (to2, raw) => {
+    let text = String(raw).split('\n').join(' ');
+    if (text.length > 500) {
+      text = `${text.slice(0, 490)} ...`;
+    }
+    client.say(to2, text);
+  };
+
+  messageObj.sayTo = say;
+  messageObj.respond = (text) => {
+    say(text);
+  };
+  messageObj.respondWithMention = (text) => {
+    say(to, `${from}, ${text}`);
   };
 
   if (to === config.nick) {
@@ -34,15 +56,44 @@ client.addListener('message', (from, to, message) => {
       prefix: config.commandPrefix,
       command,
     };
+  } else if (messageObj.pm) {
+    messageObj.command = {
+      command: message,
+    };
   }
 
   if (config.verbose) {
-    console.error(messageObj);
+    console.error(yamlStringify(messageObj, {
+      skipInvalid: true,
+      flowLevel: 2,
+      noRefs: true,
+    }).trim());
   }
 
-  plugins.forEach((plugin) => {
-    plugin(messageObj);
-  });
+  // log the message
+  if (!logs[to]) logs[to] = [];
+  logs[to].unshift(messageObj);
+  if (logs[to].length > 500) logs[to].pop();
+
+  messageObj.logs = logs[to];
+
+  messageObj.handling = (pluginName, extraInfo) => {
+    let log = '';
+    log += `${chalk.bgRed(to)}`;
+    log += ` ${chalk.bgYellow(from)}`;
+    log += ` ${chalk.bgBlue(pluginName)}`;
+    log += ` ${messageObj.message}`;
+    console.log(log);
+    if (extraInfo !== undefined) {
+      console.log(extraInfo);
+    }
+    console.log('');
+  };
+  messageObj.log = (pluginName, extraInfo) => {
+    console.log(`${pluginName}: `, extraInfo);
+  };
+
+  plugins.run(messageObj);
 });
 
 client.addListener('error', (message) => {
--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -1,3 +1,21 @@
-module.exports = [
+/* eslint global-require: "off" */
+const runPlugins = (msg) => {
+  // clear the cache so we don't need to restart the bot
+  Object.keys(require.cache).forEach((key) => {
+    delete require.cache[key];
+  });
 
-];
+  const plugins = {
+    repaste: require('./repaste/repastePlugin'),
+  };
+
+  Object.keys(plugins).forEach((key) => {
+    const msg2 = Object.assign({}, msg, {
+      handling: extraInfo => msg.handling(key, extraInfo),
+      log: extraInfo => msg.log(key, extraInfo),
+    });
+    plugins[key](msg2);
+  });
+};
+
+exports.run = runPlugins;
