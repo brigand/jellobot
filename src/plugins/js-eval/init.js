862c045588255ca5a023addf7fea8feed9707e00
caub <cyril.auburtin@gmail.com>
2018-09-29 13:11:36 +0200

    changes on docker-js-eval image, more formatting tests

--- src/plugins/js-eval/init.js
+++ src/plugins/js-eval/init.js
@@ -1,41 +1,10 @@
 const { exec } = require('child_process');
-const evalUtils = require('./evalUtils');
-
-const tryParse = (json) => {
-  try {
-    return JSON.parse(json);
-  } catch (e) {
-    return null;
-  }
-};
 
 async function initJsEval() {
   // Kill all docker containers created with "--name jseval-{some hex string}"
-  exec(`docker ps --format '{{ json . }}'`, { encoding: 'utf-8'}, (err, stdout = '') => {
-    const containers = stdout.split(/[\r\n]+/)
-      .map(x => x.trim())
-      .filter(Boolean)
-      .map(tryParse)
-      .filter(Boolean);
+  exec(`docker rm -f $(docker ps -qf name=jseval-) 2>/dev/null`, (err, stdout = '') => {
 
-    const evalContainers = containers.filter(x => evalUtils.names.test(x.Names));
-    if (!evalContainers.length) {
-      return;
-    }
-
-    console.log(`There were ${evalContainers.length} containers still running when the bot started. Killing.`);
-    evalContainers.forEach((cont) => {
-      exec(`docker kill "${cont.ID}"`, { encoding: 'utf-8' }, (killErr, killStdout) => {
-        if (killErr) {
-          console.error(`Failed to kill container`, cont);
-          console.error(' ', killErr.message);
-          console.error(killStdout.trim() + '\n\n');
-          return;
-        }
-
-        console.error(`Killed container ${cont.ID} / ${cont.Names}`);
-      });
-    });
+    console.log(`There were ${stdout.trim().split('\n').length} containers still running when the bot started. Killing them softly.`);
   });
 }
 
--- src/plugins/js-eval/jsEvalPlugin.js
+++ src/plugins/js-eval/jsEvalPlugin.js
@@ -1,15 +1,15 @@
 const { exec } = require('child_process');
-const evalUtils = require('./evalUtils');
+const crypto = require('crypto');
 
 const children = new Set();
 
-const jsEvalPlugin = async ({ mentionUser, respond, message, selfConfig, log }) => {
+const jsEvalPlugin = async ({ mentionUser, respond, message, selfConfig = { timer: 5000 }, log }) => {
   if (!message.startsWith('n>')) return;
   const code = message.slice(2);
-  const childId = evalUtils.names.make();
 
   let didRespond = false;
   let child;
+  const childId = `jseval-${crypto.randomBytes(8).toString('hex')}`;
 
   const done = (err, stdout = '') => {
     if (didRespond) {
@@ -18,31 +18,30 @@ const jsEvalPlugin = async ({ mentionUser, respond, message, selfConfig, log }) 
     didRespond = true;
     children.delete(child);
 
-    const msg = err && err.killed ? 'Timeout' : stdout.trim();
-    const prefix = mentionUser ? `${mentionUser}, ` : err ? '(error) ' : '(okay) ';
-    respond(prefix + msg);
+    const prefix = mentionUser ? `${mentionUser}, ` : err ? (err.killed ? '(timeout) ' : '(error) ') : '(okay) ';
+    respond(prefix + stdout.trim());
   };
 
   const timer = setTimeout(() => {
     done({ killed: true }, '');
     child.kill();
 
-    exec(`docker kill "${childId}"`, { encoding: 'utf-8'}, (err, stdout = '', stderr = '') => {
+    exec(`docker kill "${childId}"`, { encoding: 'utf-8' }, (err, stdout = '', stderr = '') => {
       const ignore = !err || /No such container/.test(`${stdout}\0${stderr}`);
       if (!ignore) {
         log(`Failed to kill docker container ${name}\n${stdout}\n${stderr}`);
       }
     });
-  }, selfConfig.timer || 5000);
+  }, 1.5 * selfConfig.timer || 5000);
 
-  child = exec(`docker run --rm -i --net=none --name=${childId} devsnek/js-eval`, { encoding: 'utf-8'}, (err, stdout = '') => {
+  child = exec(`docker run --rm -i --net=none --name=${childId} -eJSEVAL_ENV=node-cjs devsnek/js-eval`, { encoding: 'utf-8', timeout: selfConfig.timer || 5000 }, (err, stdout = '') => {
     clearTimeout(timer);
     done(err, stdout);
   });
 
   children.add(child);
 
-  child.stdin.write(JSON.stringify({ environment: 'node-cjs', code }));
+  child.stdin.write(code);
   child.stdin.end();
 };
 
