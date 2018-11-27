7a74876e121f3bef76cb5fee9c7edc356be0a663
Frankie Bagnardi <f.bagnardi@gmail.com>
2018-11-27 11:43:16 -0700

    rng plugin anchors command regex (prevents !foo pick 1 2 3)

--- src/plugins/rng/rngPlugin.js
+++ src/plugins/rng/rngPlugin.js
@@ -2,7 +2,7 @@
 const rngPlugin = async ({ command, handling = () => { }, respondWithMention }) => {
   if (!command) return;
 
-  const m = command.command.match(/(?:pick|choose|which)[: ](\S[^#?]*)/); // allow to pass comments after # or ?
+  const m = command.command.match(/^(?:pick|choose|which)[: ](\S[^#?]*)/); // allow to pass comments after # or ?
 
   if (m) {
     const args = m[1].trim().split(/(?:or|[\s,])+/i); // split by stop words
