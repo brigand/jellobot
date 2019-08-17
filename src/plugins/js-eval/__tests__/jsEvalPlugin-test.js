e05efb2e4b95bc509b17b22fe43fd59113fca254
caub <cyril.auburtin@gmail.com>
2019-08-18 00:36:46 +0200

    drop date-fns, not necessary

--- src/plugins/js-eval/__tests__/jsEvalPlugin-test.js
+++ src/plugins/js-eval/__tests__/jsEvalPlugin-test.js
@@ -71,11 +71,6 @@ describe('jsEvalPlugin', () => {
     expect(output).toEqual(`(okay) 3n`);
   });
 
-  it('has date-fns', async () => {
-    const output = await testEval(`n> require('date-fns').subDays(new Date(2018,10,5), 10)`);
-    expect(output).toEqual(`(okay) 2018-10-26T00:00:00.000Z`);
-  });
-
   it(`babel has String.prototype.matchAll`, async () => {
     const output = await testEval(`b> [...'1 2 3'.matchAll(/\\d/g)].map(o => o.index)`);
     expect(output).toEqual(`(okay) [ 0, 2, 4 ]`);
